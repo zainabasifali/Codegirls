219dd776cc1a9db2305ba53626c09d35d41ffb79
zainabasifali <zainabasif5312@gmail.com>
2023-12-28 20:47:08 +0500

    Uploading Day5 folder

---
+++ Day5/day5.js
@@ -0,0 +1,8 @@
+function Logged(name) {
+  if(name == "Zainab"){
+      return name;
+  }
+  }
+
+module.exports = Logged;
+
---
+++ Day5/index.js
@@ -0,0 +1,5 @@
+function make_changes(){
+    var authenticate = require('./day5.js');
+    console.log(authenticate("Zainab")+ " Logged Sucessfully");
+}
+make_changes();
